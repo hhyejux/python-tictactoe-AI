8ec0100993bab385e3b4ed884f329f726b2e9174
kooock <koock1994@gmail.com>
2020-11-25 02:23:40 +0900

    it's wrote ai's choose

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import json
 
 app = Flask(__name__)
 
+x = 1
+o = 3
 
 lotto_numbers = list(range(1,46))
 
@@ -25,8 +27,137 @@ def choice():
 def computer_hard(positions, moves):
     res = {"choose" : -1}
 
+    if moves == 0:
+        res["choose"] = 4
+        return res
+
+    if moves == 2:
+        if (positions[0] == o) or (positions[1] == o):
+            res["choose"] = 2
+        elif (positions[2] == o) or (positions[5] == o):
+            res["choose"] = 8
+        elif (positions[7] == o) or (positions[8] == o):
+            res["choose"] = 6
+        else:
+            res["choose"] = 0
+        return res
+
+    if moves == 4:
+        if positions[2] == x:
+            if positions[6] == o:
+                if positions[0] == o:
+                    res["choose"] = 3
+                else:
+                    res["choose"] = 5
+            else:
+                res["choose"] = 6 # 승
+
+        elif positions[8] == x:
+            if positions[0] == o:
+                if positions[2] == o:
+                    res["choose"] = 1
+                else:
+                    res["choose"] = 7
+            else:
+                res["choose"] = 0
+        elif positions[6] == x:
+            if positions[2] == o:
+                if positions[8] == o:
+                    res["choose"] = 5
+                else:
+                    res["choose"] = 3
+            else:
+                res["choose"] = 2
+        # positions[0] == x
+        else:
+            if positions[8] == o:
+                if positions[6] == o:
+                    res["choose"] = 7
+                else:
+                    res["choose"] = 1
+            else:
+                res["choose"] = 8
+        return res
+
+    if moves == 6:
+        if positions[2] == x:
+            if positions[5] == x:
+                if positions[3] == o:
+                    res["choose"] = 8 # 승
+                else:
+                    res["choose"] = 3 # 승
+            else: # positions[3] == x
+                if positions[5] == o: # 막는 경우
+                    res["choose"] = 7
+                else: # 안 막는 경우
+                    res["choose"] = 5 # 승
+
+        elif positions[8] == x:
+            if positions[7] == x:
+                if positions[1] == o:
+                    res["choose"] = 6 # 승
+                else:
+                    res["choose"] = 1 # 승
+            else: # positions[3] == x
+                if positions[7] == o: # 막는 경우
+                    res["choose"] = 3
+                else: # 안 막는 경우
+                    res["choose"] = 7 # 승
+        elif positions[6] == x:
+            if positions[3] == x:
+                if positions[5] == o:
+                    res["choose"] = 0 # 승
+                else:
+                    res["choose"] = 5 # 승
+            else: # positions[3] == x
+                if positions[3] == o: # 막는 경우
+                    res["choose"] = 1
+                else: # 안 막는 경우
+                    res["choose"] = 3 # 승
+        else:
+            if positions[1] == x:
+                if positions[7] == o:
+                    res["choose"] = 2 # 승
+                else:
+                    res["choose"] = 7 # 승
+            else: # positions[3] == x
+                if positions[1] == o: # 막는 경우
+                    res["choose"] = 5
+                else: # 안 막는 경우
+                    res["choose"] = 1 # 승
 
         return res
+
+    if moves == 8:
+        if positions[2] == x:
+            if positions[1] == o:
+                res["choose"] = 8 # 무승부
+            else:
+                res["choose"] = 1
+        elif positions[8] == x:
+            if positions[5] == o:
+                res["choose"] = 6 # 무승부
+            else:
+                res["choose"] = 5
+        elif positions[6] == x:
+            if positions[7] == o:
+                res["choose"] = 0 # 무승부
+            else:
+                res["choose"] = 7
+        else:
+            if positions[3] == o:
+                res["choose"] = 2 # 무승부
+            else:
+                res["choose"] = 3
+        #x = 1
+        #o = 3
+
+        #0 1 2
+        #3 4 5
+        #6 7 8
+        return res
+
+
 
 
 if __name__ == "__main__":
